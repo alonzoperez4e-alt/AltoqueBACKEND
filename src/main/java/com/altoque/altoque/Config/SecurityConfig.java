d78c5392714f798b9052ce1d7216286837cc6151
Alonzo Pérez Cristhian <laespiga0210@gmail.com>
2025-10-15 19:16:14 -0500

    Fix CORS: permitir dominio de Vercel en producción

--- src/main/java/com/altoque/altoque/Config/SecurityConfig.java
+++ src/main/java/com/altoque/altoque/Config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration cors = new CorsConfiguration();
-        cors.setAllowedOriginPatterns(List.of("http://localhost:8081")); // tu frontend
+        cors.setAllowedOriginPatterns(List.of("https://altoque-frontend-oep9j9as0-laespigas-projects.vercel.app")); // tu frontend
         cors.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         cors.setAllowedHeaders(List.of("*"));
         cors.setAllowCredentials(true);
