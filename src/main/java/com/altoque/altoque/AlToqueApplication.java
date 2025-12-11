03e1fc3a66b7f027527d04b769c0bd779ed1fe4f
Alonzo Pérez Cristhian <laespiga0210@gmail.com>
2025-12-10 19:28:24 -0500

    Ultimos cambios

--- src/main/java/com/altoque/altoque/AlToqueApplication.java
+++ src/main/java/com/altoque/altoque/AlToqueApplication.java
@@ -1,13 +1,23 @@
 package com.altoque.altoque;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class AlToqueApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AlToqueApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        // Configurar la zona horaria por defecto a Perú (UTC-5)
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Lima"));
+        System.out.println("Hora actual configurada: " + new java.util.Date());
     }
 
 }
